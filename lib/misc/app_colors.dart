d1567bae3d2f312165386f6fa559434728fbb1df
Glazynov A <glazunov_ay@lemz-t.ru>
2020-08-18 22:34:11 +0700

    add app colors

---
+++ lib/misc/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color backgroundColor = Color(0xFF47595C);
+  static final Color subBackgroundColor = Color(0xFF3F4C4F);
+  static final Color numberColor = Color(0xFFD6D9D9);
+  static final Color operatorColor = Color(0xFF81C188);
+
+  static final LinearGradient backgroundGreenGradient = new LinearGradient(
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight,
+      colors: [Color(0xFF58BC7B), Color(0xFFA0DC71)]);
+}
